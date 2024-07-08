fa7258878b5354ccf74b538b1b0fc1908fb8cf44
Jose Alves <jose.alves@mynaric.com>
2024-07-08 17:05:26 +0200

    update index exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,12 @@
 import { DataTable, getSummary } from "./components/DataTable";
 import { getCurrency, getNumformat } from "./utils/formatters";
+import { dateRangeFilterFn, booleanFilterFn } from "./utils/filters";
 
-export { DataTable, getSummary, getCurrency, getNumformat }
+export {
+  DataTable,
+  getSummary,
+  getCurrency,
+  getNumformat,
+  dateRangeFilterFn,
+  booleanFilterFn,
+};
