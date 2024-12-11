69176d8e676a5928035d1860ff514d9462769062
Jose Alves <jose.alves@mynaric.com>
2024-12-11 16:13:22 +0100

    remove unused code

--- src/lib/utils/formatters.ts
+++ src/lib/utils/formatters.ts
@@ -19,20 +19,3 @@ export const getNumformat = (n: number) => {
   };
   return n.toLocaleString("en-US", options);
 };
-
-export function parseValueIntoString(value: any): string {
-  if (value instanceof Date) {
-    // If the value is a Date, convert it to a locale string
-    return value.toLocaleString();
-  } else if (Array.isArray(value)) {
-    // If the value is an array, recursively parse each element and join with commas
-    return value.map((element) => parseValueIntoString(element)).join(", ");
-  } else if (value instanceof Object) {
-    // If the value is an object, check for "default" property or parse its values
-    return value.hasOwnProperty("default")
-      ? value["default"] // Assume user set it to a string
-      : parseValueIntoString(Object.values(value)[0]);
-  }
-  // If the value is neither Date, Object, nor Array, return it as-is
-  return value;
-}
